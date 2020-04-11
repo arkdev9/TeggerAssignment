b1e6554ffba7bd42047c9246d9006629317e3cfb
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-04-11 21:52:36 +0530

    Updates

---
+++ precache-manifest.c954d93df5b555ea492f93c172c65d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d87f4999d82442ec63a0c2ab7982375",
+    "url": "/github.io/trubo-train/index.html"
+  },
+  {
+    "revision": "e8cad55a90786c5200df",
+    "url": "/github.io/trubo-train/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e8cad55a90786c5200df",
+    "url": "/github.io/trubo-train/static/js/2.f3ff0760.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github.io/trubo-train/static/js/2.f3ff0760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e355f6d9175f24a0e8",
+    "url": "/github.io/trubo-train/static/js/main.9ba2b900.chunk.js"
+  },
+  {
+    "revision": "bdc8f46132f8897b4d29",
+    "url": "/github.io/trubo-train/static/js/runtime-main.238230f5.js"
+  }
+]);
