6197ca5c82468eb603e7f0e146227a1789f699d9
Abhishek Kasireddy <Akasireddy99@gmail.com>
2020-04-11 13:36:28 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+
+function App() {
+	return (
+		<Row>
+			<Col>
+				<h1>Hello World</h1>
+			</Col>
+		</Row>
+	);
+}
+
+export default App;
